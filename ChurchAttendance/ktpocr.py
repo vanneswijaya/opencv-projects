a0cb1506c3769aaf1f9f2029f9da49a73a5b8c5a
rafaelvannes <vannesoey@gmail.com>
2020-06-13 12:43:09 +0700

    SignUp and KTP Detection

---
+++ ChurchAttendance/ktpocr.py
@@ -0,0 +1,66 @@
+from cv2 import cv2
+import numpy as np
+import pytesseract
+import matplotlib.pyplot as plt
+from PIL import Image
+
+class Ktp:
+    def __init__(self, name, img_path):
+        self.name = name
+        self.img_path = img_path
+
+    def ktp_to_string(self):
+        tessdata_dir_config = r'--tessdata-dir "/usr/local/Cellar/tesseract/4.1.1/share/tessdata"'
+
+        img = cv2.imread(self.img_path)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        th, threshed = cv2.threshold(gray, 127, 255, cv2.THRESH_TRUNC)
+
+        result = pytesseract.image_to_string((threshed), lang="ind", config = tessdata_dir_config)
+
+        return result
+
+    def char_replace(self):
+        result = self.ktp_to_string()
+        teks = ''
+
+        for word in result.split("\n"):
+            if "”—" in word:
+                word = word.replace("”—", ":")
+
+            if "NIK" in word:
+                nik_char = word.split()
+                if "D" in word:
+                    word = word.replace("D", "0")
+                if "?" in word:
+                    word = word.replace("?", "7") 
+        
+            teks += word
+            teks += '\n'
+
+        return teks
+
+    def name_check(self):
+        result = self.ktp_to_string()
+
+        for word in result.split("\n"):
+            if self.name.upper() in word:
+                return True
+            else:
+                continue
+        return False
+
+    def validate_ktp(self):
+        word = self.char_replace()
+
+        if ("NIK" in word and "Agama" in word and 'Pekerjaan' in word):
+            if self.name_check():
+                return True
+            else:
+                return False
+        else:
+            return False
+
+# ktp = Ktp('ISAAC SJAHRIR', 'dataset/ktp9.jpg')
+# print(ktp.char_replace())
---
+++ ChurchAttendance/signup.py
@@ -0,0 +1,69 @@
+import pymongo
+from pymongo import MongoClient
+import uuid
+import boto3
+from ktpocr import Ktp
+
+cluster = MongoClient('mongodb+srv://vannes:mysonmyson@cluster0-w3igv.mongodb.net/katedral?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE')
+db = cluster['katedral']
+collection = db['test']
+
+class SignUp:
+    def __init__(self, name, ktp):
+        self.name = name
+        self.ktp = ktp
+
+    @staticmethod
+    def get_filename():
+        x = uuid.uuid1()
+        return str(x)+ '.jpg'
+
+    @classmethod
+    def ask_input(cls):
+        name = input('Masukkan nama (sesuai yang tertera di KTP) : ')
+        ktp = input('Kirimkan foto KTP anda : ')
+        x = Ktp(name, ktp)
+        if Ktp.validate_ktp(x):
+            return cls(name, ktp)
+        else:
+            return SignUp.ask_input()
+
+    def upload_image(self, source_filename):
+        filename = self.get_filename()
+        neo_bucket = 'frproject'
+        neo_session = boto3.Session(aws_access_key_id='00cb61562dc3a050d384', aws_secret_access_key='xQQUOjm3cJEpEJ8+jZpLosW0vL8FOJIa9UNeOetU')
+        neo_s3 = neo_session.client('s3', endpoint_url='https://nos.jkt-1.neo.id')
+
+        s3_path = 'images'
+        destination_filename = filename
+        source_filename = source_filename
+
+        s3_fullname = f'{s3_path}/{destination_filename}'
+
+        neo_s3.upload_file(source_filename, neo_bucket, s3_fullname, ExtraArgs={'ACL': 'public-read', 'ContentType':'image/png'})
+        url_neo = f'{neo_s3.meta.endpoint_url}/{neo_bucket}/{s3_fullname}'
+        
+        return url_neo
+
+    def store_db(self):
+        img_url = self.upload_image(self.ktp)
+        x = Ktp(self.name, self.ktp)
+        ktp_data = x.char_replace()
+        post = {
+            'name':self.name,
+            'image_url':img_url,
+            'data_ktp':ktp_data
+            }
+        collection.insert_one(post)
+        print('Data stored')
+
+    @staticmethod
+    def run():
+        ask = SignUp.ask_input()
+        ask.store_db()
+        
+SignUp.run()
+
+
+
+    
